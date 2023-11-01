7e8f3269fbce15f0ac42e2f902ba49835c959823
DizBars <DizBars@yandex.ru>
2023-11-01 16:38:09 +0300

    Day 3 Full HTML

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,9 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import {initAccordions} from './modules/accordion/init-accordion';
+import {initTabs} from './modules/tabs/init-tabs';
+import './modules/tabs/tabs';
+import './modules/accordion/accordions';
 
 // ---------------------------------
 
@@ -17,10 +20,11 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
     const form = new Form();
     window.form = form;
     form.init();
+    initAccordions();
+    initTabs();
   });
 });
 
--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,4 +1,27 @@
 // Swiper 8.4.7
-// import './vendor/swiper';
+import './vendor/swiper';
 import './vendor/focus-visible-polyfill';
 
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+export default {swiper};
